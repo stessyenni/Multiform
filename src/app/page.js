7ee2315e60cf6435e00a3ca71ae653008fcb8d52
Stessy Enni <50053328+stessyenni@users.noreply.github.com>
2026-01-20 15:47:42 +0100

    new

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,7 @@ import SidebarSteps from "@/components/sidebarSteps";
 export default function App() {
   return (
     <main className="flex h-screen items-center justify-center px-10 py-5 gap-15 bg-zinc-50 font-sans">
-      {/* <SidebarSteps /> */}
-
+      <SidebarSteps />
       <MultiForm />
     </main>
   );
